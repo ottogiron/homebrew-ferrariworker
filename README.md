55198f0e03e4fbc02f48d938ac2a5ed9a00c72eb
Otto Giron <otto.giron@xoom.com>
2016-09-17 21:41:49 -0600

    Add bumper

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Homebrew Ferrariworker
 
 Mac OS Homebrew formula for ferrariworker
+
+
+## Bump
+
+To bump to the next version run
+
+```bash
+sh bump.sh <version> <binary_tar_sha256>
+```
--- bump.sh
+++ bump.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 VERSION=$1
 SHA256=$2
 URL=https://github.com/ottogiron/ferrariworker/releases/download/$VERSION/ferrariworker.darwin-amd64.tar.gz 
-sh <(curl https://raw.githubusercontent.com/ottogiron/my-go-homebrew-bumper/master/bumper.sh) $VERSION $SHA256 $URL ferrariworker.rb
+
+curl -L  https://raw.githubusercontent.com/ottogiron/my-go-homebrew-bumper/master/bumper.sh | sh -s $VERSION $SHA256 $URL ferrariworker.rb
