200c515017fa60db85160fa081625e3467619c29
Otto Giron <otto.giron@xoom.com>
2016-09-17 21:51:53 -0600

    Bump to 0.6.0

--- ferrariworker.rb
+++ ferrariworker.rb
@@ -3,7 +3,7 @@ class Ferrariworker < Formula
   homepage "https://github.com/ottogiron/ferrariworker"
   url "https://github.com/ottogiron/ferrariworker/releases/download/0.6.0/ferrariworker.darwin-amd64.tar.gz"
   version "0.6.0"
-  sha256 "af14c8059a91ba60abb4388c7a285d12de21b5b029fb3a587400e8e0b7d19732"
+  sha256 "ff155cb7c15bd4e7935a17d5ce583ea16f2659a94630378c6a152374821edd88"
 
   def install
    bin.install('ferrariworker')
