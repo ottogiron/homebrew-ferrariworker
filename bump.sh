d29b8c7ff257a8e685c5c74faf87ce4a9a7b166b
Otto Giron <otto.giron@xoom.com>
2016-09-17 21:05:46 -0600

    Add bump.sh

--- bump.sh
+++ bump.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
 VERSION=$1
 SHA256=$2
-sed -e "s/version.*/version \"$VERSION\"/" \
-    -e "s_url.*_url \"https://github.com/ottogiron/ferrariworker/releases/download/$VERSION/ferrariworker.darwin-amd64.tar.gz\"_" \
-    -e "s/sha256.*/sha256 \"$SHA256\"/" \
-    ferrariworker.rb
+URL=https://github.com/ottogiron/ferrariworker/releases/download/$VERSION/ferrariworker.darwin-amd64.tar.gz 
+sh <(curl https://raw.githubusercontent.com/ottogiron/my-go-homebrew-bumper/master/bumper.sh) $VERSION $SHA256 $URL ferrariworker.rb
